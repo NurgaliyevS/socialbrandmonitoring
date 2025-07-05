706b2a4e73bdf6512db83c74595683816ec17e2a
NurgaliyevS <nurgasab@gmail.com>
2025-07-05 09:53:25 +0500

    upd

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,10 +2,12 @@ import Hero from "@/components/Hero";
 import ProblemSolution from "@/components/ProblemSolution";
 import Features from "@/components/Features";
 import Pricing from "@/components/Pricing";
+import NavigationHeader from "@/components/NavigationHeader";
 
 export default function Home() {
   return (
     <div className="min-h-screen">
+      <NavigationHeader />
       <Hero />
       <ProblemSolution />
       <Features />
